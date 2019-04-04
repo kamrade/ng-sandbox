a9e2d26702cfe98667fe6ac4bab836e361ebf649
Denis Mikhailov <denis.michailov@gmail.com>
2019-04-04 15:51:22 +1000

    Fix pipe-widget tests

--- src/app/pages/manipulating-the-ui/pipe-widget/pipe-widget.component.spec.ts
+++ src/app/pages/manipulating-the-ui/pipe-widget/pipe-widget.component.spec.ts
@@ -1,6 +1,8 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { PipeWidgetComponent } from './pipe-widget.component';
+import { DynamicCasePipe } from 'src/app/pages/manipulating-the-ui/pipe-widget/dynamic-case.pipe';
+import { SharedComponentsModule } from 'src/app/shared-components/shared-components.module';
 
 describe('PipeWidgetComponent', () => {
   let component: PipeWidgetComponent;
@@ -8,7 +10,8 @@ describe('PipeWidgetComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ PipeWidgetComponent ]
+      declarations: [ PipeWidgetComponent, DynamicCasePipe ],
+      imports: [SharedComponentsModule]
     })
     .compileComponents();
   }));
