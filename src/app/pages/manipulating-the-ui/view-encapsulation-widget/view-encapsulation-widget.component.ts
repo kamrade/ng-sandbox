648b5822b2bb2f6eae8d6a31b7c8fc5e28d68e19
Denis Michailov <kamrad.reg@gmail.com>
2019-08-28 21:10:38 +1000

    save

--- src/app/pages/manipulating-the-ui/view-encapsulation-widget/view-encapsulation-widget.component.ts
+++ src/app/pages/manipulating-the-ui/view-encapsulation-widget/view-encapsulation-widget.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewEncapsulation} from '@angular/core';
 
 @Component({
   selector: 'app-view-encapsulation-widget',
   templateUrl: './view-encapsulation-widget.component.html',
-  styleUrls: ['./view-encapsulation-widget.component.scss']
+  styleUrls: ['./view-encapsulation-widget.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class ViewEncapsulationWidgetComponent implements OnInit {
   constructor() { }
